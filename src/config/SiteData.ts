bb6f8dd1703f921999a1ee90282e03b7347f9fb9
adell-fael <adel0fael@gmail.com>
2025-06-21 16:33:25 +0300

    adding some links

--- src/config/SiteData.ts
+++ src/config/SiteData.ts
@@ -1,8 +1,8 @@
 const SiteData = {
 	title: 'Abu Al Yaman',
 	description: 'Restaurant Menu',
-	whatsapp: '',
-	facebook: '',
+	whatsapp: 'https://wa.me/+9630998937567?text=Hello%And%Welcome',
+	facebook: 'https://www.facebook.com/61576260862666/',
 	instagram: 'https://www.instagram.com/abualyaman23/',
 }
 
